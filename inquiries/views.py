e8f42f297f525b6797ee6d92b896cbe5409b448f
SFaubion <mbuller10@gmail.com>
2015-04-27 18:02:38 -0700

    more url stuff

--- inquiries/views.py
+++ inquiries/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the inquiries index")
+
+def detail(request, starq_id):
+    return HttpResponse("You're looking at question %s." % starq_id)
+
+def results(request, starq_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % starq_id)
+
+def vote(request, starq_id):
+    return HttpResponse("You're voting on question %s." % starq_id)
